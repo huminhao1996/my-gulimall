cb2e986af62a1de5d10f5a60a4ed9d4409b5df00
Nagisaki <454977355@qq.com>
2021-11-28 22:10:34 +0800

    整合redis,redission分布式锁,spring-cache

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,7 +3,9 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 1、整合MyBatis-Plus
@@ -54,8 +56,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 
 //@EnableRedisHttpSession     //开启springsession
-//@EnableCaching      //开启缓存功能
-//@EnableFeignClients(basePackages = "com.atguigu.gulimall.product.feign")
+@EnableCaching      //开启缓存功能
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.product.feign")
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication //(exclude = GlobalTransactionAutoConfiguration.class)
